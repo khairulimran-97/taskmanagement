6099638d351b7191f53d3d57c554fa63e68928e5
Khairul <info@khairulimran.com>
2025-05-25 20:41:30 +0800

    Update Index.vue

--- resources/js/pages/Notes/Index.vue
+++ resources/js/pages/Notes/Index.vue
@@ -575,7 +575,7 @@ onMounted(() => {
                             v-model="noteForm.content"
                             :editable="true"
                             placeholder="Start writing your note... (Type '/' for commands)"
-                            class="w-full h-screen rounded-none"
+                            class="w-full h-full rounded-none"
                             @update:modelValue="(value) => noteForm.content = value"
                         />
                     </div>
